628c696c5df8a724b969b2ebde62dd1ba5d74fd9
Opilane TTHK <opilane@tthk.ee>
2023-03-30 17:36:52 +0300

    houseDomainDto

---
+++ TARge21House/TARge21House.Core/Domain/House.cs
@@ -0,0 +1,17 @@
+﻿namespace TARge21House.Core.Domain
+{
+    public class House
+    {
+        public Guid Id { get; set; }
+        public string Address { get; set; }
+        public int Size { get; set; }
+        public int Price { get; set; }
+        public int Rooms { get; set; }
+
+        // database only
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+
+    }
+}
---
+++ TARge21House/TARge21House.Core/Dto/HouseDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARge21House.Core.Dto
+{
+    public class HouseDto
+    {
+        public Guid Id { get; set; }
+        public string Address { get; set; }
+        public int Size { get; set; }
+        public int Price { get; set; }
+        public int Rooms { get; set; }
+
+        // database only
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
