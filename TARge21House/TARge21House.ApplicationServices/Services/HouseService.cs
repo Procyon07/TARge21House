be25667032bf65e4de3e44017f8b367815d9f87a
Karl-Martin <kmkyngas1@gmail.com>
2023-04-02 15:13:09 +0300

    ServiceCreate

--- TARge21House/TARge21House.ApplicationServices/Services/HouseService.cs
+++ TARge21House/TARge21House.ApplicationServices/Services/HouseService.cs
@@ -1,14 +1,37 @@
 ﻿using TARge21House.Core.Domain;
 using TARge21House.Core.Dto;
 using TARge21House.Core.ServiceInterface;
+using TARge21House.Data;
 
 namespace TARge21House.ApplicationServices.Services
 {
-    internal class HouseService : IHouseService
+    public class HouseService : IHouseService
     {
-        public Task<House> Create(HouseDto dto)
+        private readonly TARge21HouseContext _context;
+
+        public HouseService
+            (
+                TARge21HouseContext context
+            )
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+        public async Task<House> Create(HouseDto dto)
+        {
+            House house = new House();
+
+            house.Id = Guid.NewGuid();
+            house.Address = dto.Address;
+            house.Size = dto.Size;
+            house.Price = dto.Price;
+            house.Rooms = dto.Rooms;
+            house.CreatedAt = dto.CreatedAt;
+            house.ModifiedAt = dto.ModifiedAt;
+
+            await _context.Houses.AddAsync(house);
+            await _context.SaveChangesAsync();
+
+            return house;
         }
 
         public Task<House> Delete(Guid id)
---
+++ TARge21House/TARge21House/Models/House/HouseIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace TARge21House.Models.House
+{
+    public class HouseIndexViewModel
+    {
+        public Guid? Id { get; set; }
+        public string Address { get; set; }
+        public int Size { get; set; }
+        public int Price { get; set; }
+        public int Rooms { get; set; }
+
+        // database only
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
