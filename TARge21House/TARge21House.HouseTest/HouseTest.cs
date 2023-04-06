b7f1f4eed847f572b98bca8f74d04182b0dadb49
Opilane TTHK <opilane@tthk.ee>
2023-04-06 13:28:51 +0300

    Delete&GetTests

--- TARge21House/TARge21House.HouseTest/HouseTest.cs
+++ TARge21House/TARge21House.HouseTest/HouseTest.cs
@@ -1,7 +1,6 @@
 using TARge21House.Core.Dto;
 using TARge21House.Core.ServiceInterface;
 using TARge21House.Core.Domain;
-using Xunit;
 
 namespace TARge21House.HouseTest
 {
@@ -14,7 +13,6 @@ namespace TARge21House.HouseTest
 
             HouseDto house = new HouseDto();
 
-            //spaceship.Id = Guid.Parse(guid);
             house.Address = "asd";
             house.Price = 123;
             house.Size = 123;
@@ -25,6 +23,69 @@ namespace TARge21House.HouseTest
             var result = await Svc<IHouseService>().Create(house);
 
             Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task ShouldNot_GetByIdHouse_WhenReturnsNotEqual()
+        {
+            //Arrange
+            Guid wrongGuid = Guid.Parse(Guid.NewGuid().ToString());
+            Guid guid = Guid.Parse("6e8285f6-5205-46d4-b12e-c522f797f378");
+
+            //Act
+            await Svc<IHouseService>().GetAsync(guid);
+
+            //Assert
+            Assert.NotEqual(wrongGuid, guid);
+        }
+
+        [Fact]
+        public async Task Should_GetByIdHouse_WhenReturnsEqual()
+        {
+            Guid databaseGuid = Guid.Parse("6e8285f6-5205-46d4-b12e-c522f797f378");
+            Guid getGuid = Guid.Parse("6e8285f6-5205-46d4-b12e-c522f797f378");
+
+            await Svc<IHouseService>().GetAsync(getGuid);
+
+            Assert.Equal(databaseGuid, getGuid);
+        }
+
+        [Fact]
+        public async Task Should_DeleteByIdHouse_WhenDeleteHouse()
+        {
+            HouseDto house = MockHouseData();
+
+            var addHouse = await Svc<IHouseService>().Create(house);
+            var result = await Svc<IHouseService>().Delete((Guid)addHouse.Id);
+
+            Assert.Equal(result, addHouse);
+        }
+
+        [Fact]
+        public async Task ShouldNot_DeleteByIdSpaceship_WhenDidNotDeleteSpaceship()
+        {
+            HouseDto house = MockHouseData();
+            var addHouse = await Svc<IHouseService>().Create(house);
+            var addHouse2 = await Svc<IHouseService>().Create(house);
+
+            var result = await Svc<IHouseService>().Delete((Guid)addHouse2.Id);
+
+            Assert.NotEqual(result.Id, addHouse.Id);
+        }
+
+        private HouseDto MockHouseData()
+        {
+            HouseDto house = new()
+            {
+                Address = "hhhhh",
+                Price = 123,
+                Size = 123,
+                Rooms = 123,
+                CreatedAt = DateTime.Now,
+                ModifiedAt = DateTime.Now,
+            };
+
+            return house;
         }
     }
 }
